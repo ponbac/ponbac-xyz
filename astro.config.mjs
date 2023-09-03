83278aafd3291fa4c07f937db7a5b4ac4cb914a6
ponbac <root@ponbac.xyz>
2023-09-03 10:00:04 +0200

    smth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ponbac.xyz',
-  integrations: [tailwind(), sitemap(), mdx()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), sitemap(), mdx(), react()]
 });
---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'ponbac.xyz';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
